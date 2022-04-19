24e418ce0e30f0cf8ac7ec6bc8dde5e5dde48e1d
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-04-19 13:06:56 +0200

    adding-bootstrap,axios libraries

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+      
+  }, []);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
