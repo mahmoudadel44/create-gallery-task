fe2d09a9bc2d8511c4f8be4518b204c59effb615
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-04-19 13:18:10 +0200

    get-images-data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
   const [images, setImages] = useState([]);
 
+  const getImages = async () => {
+    const allImages = await axios.get(
+      "https://scaleflex.cloudimg.io/v7/0.fe_task_static/pictures.json?vh=7a646d&func=proxy"
+    );
+    setImages(allImages.data);
+  };
   useEffect(() => {
-      
+    getImages();
   }, []);
-
+  console.log("images", images);
   return (
     <section>
       <div className="container">
-        <div className="row"></div>
+        <div className="row">
+          {images.map((data) => (
+            <div className="col-md-6 mb-3">
+              <img src={data.url} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
