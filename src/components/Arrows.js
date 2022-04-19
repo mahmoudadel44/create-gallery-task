c85ab91c82a91ee85fd6cf4b3cfb1ea403da6d0a
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-04-19 22:11:51 +0200

    adding-styles-carousel

---
+++ src/components/Arrows.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import "../styles/Arrows.css";
+// const Arrows = ({ handleMoveToLeft }) => {
+//   return (
+//     <>
+//       <div className="arrowLeft" onClick={handleMoveToLeft}>
+//         {/* <div>
+//           <i class="fa-solid fa-chevron-left fa-3x"></i>
+//         </div> */}
+//         Prevvvvvvvvvvvvvvvvvv
+//       </div>
+//       {/* <div className="arrowLeft" onClick={handleMoveToLeft}>
+//         <div>
+//           <i class="fa-solid fa-chevron-left fa-3x"></i>
+//         </div>
+//       </div> */}
+//     </>
+//   );
+// };
+
+// export default Arrows;
--- src/components/SingleImg.js
+++ src/components/SingleImg.js
@@ -3,33 +3,62 @@ import React, { useState } from "react";
 //Components
 import CustomModal from "./CustomModal";
 const SingleImg = ({ data, index, length }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(null);
   const [currentImg, setCurrentImg] = useState(null);
   const [currentTitle, setCurrentTitle] = useState(null);
+  const [allData] = useState(data);
   const handleClick = (data, index) => {
     setCurrentIndex(index);
     setCurrentImg(data.url);
     setCurrentTitle(data.name);
   };
 
-  const handleMoveToLeft = () => {
-    //to check if it is the last item
-    if (currentIndex + 1 >= length) {
-      setCurrentIndex(0);
-      setCurrentImg(data[0].url);
-      return;
-    }
+    const handleMoveToLeft = () => {
+      //to check if it is the last item
+      const totalLength = data.length;
+      if (currentIndex + 1 >= totalLength) {
+        setCurrentIndex(0);
+        setCurrentImg(data[0].url);
+        return;
+      }
 
-    const newIndex = currentIndex + 1;
-    //return an array with the new Img
-    const newItem = data.filter((item) => data.indexOf(item) === newIndex);
-    console.log(newItem);
-    const newImg = newItem[0].url;
+      const newIndex = currentIndex + 1;
+      //return an array with the new Img
+      const newItem = data.filter((item) => {
+        return data.indexOf(item) === newIndex;
+      });
 
-    //to set the new index and new img
-    setCurrentIndex(newIndex);
-    setCurrentImg(newImg);
-  };
+      const newImg = newItem[0].url;
+
+      //to set the new index and new img
+      setCurrentIndex(newIndex);
+      setCurrentImg(newImg);
+    };
+
+
+
+
+
+
+
+    
+//   const handleMoveToLeft = () => {
+//     const totalLength = allData.length;
+//     if (currentIndex + 1 >= totalLength) {
+//       setCurrentIndex(0);
+//       const newUrl = allData[0].url;
+//       setCurrentImg(newUrl);
+//       return;
+//     }
+//     const newIndex = currentIndex + 1;
+//     const newUrl = allData.filter((item) => {
+//       return allData?.indexOf(item) === newIndex;
+//     });
+//     const newItem = newUrl[0].url;
+//     setCurrentImg(newItem);
+//     setCurrentIndex(newIndex);
+//   };
+
   return (
     <>
       <div className="col-md-6 mb-3">
@@ -51,6 +80,7 @@ const SingleImg = ({ data, index, length }) => {
           handleMoveToLeft={handleMoveToLeft}
           index={index}
           length={length}
+          setCurrentImg={setCurrentImg}
         />
       )}
     </>
